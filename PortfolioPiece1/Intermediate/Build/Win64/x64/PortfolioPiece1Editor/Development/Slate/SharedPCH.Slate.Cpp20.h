20feaefe44a880f2da1e6a8ff53080584599a221
RainyFluff <nikodemius.ivarsson@elev.ga.lbs.se>
2024-12-14 18:30:55 +0100

    Cleanse GITIGNORED files

--- PortfolioPiece1/Intermediate/Build/Win64/x64/PortfolioPiece1Editor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Users/nikodemius.ivarsson/Documents/GitHub/Portfolio1/PortfolioPiece1/Intermediate/Build/Win64/x64/PortfolioPiece1Editor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
